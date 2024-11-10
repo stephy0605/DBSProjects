62027251dbca1b3f9dd3957ecd3ee70804267ffd
stephy <stephyjohn0605@gmail.com>
2024-11-10 16:00:11 +0000

    v1

---
+++ Terraform_Scripts/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+}
+
+output "public_key" {
+  value = azapi_resource_action.ssh_public_key_gen.output.publicKey
+}
+
+output "private_key" {
+  value = azapi_resource_action.ssh_public_key_gen.output.privateKey
+}
