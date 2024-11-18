937bcf24a7026ddeb6fe41fd6ff210b58c57474e
stephy <stephyjohn0605@gmail.com>
2024-11-18 12:51:47 +0000

    Update

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def green_screen():
-    return '<html><body style="background-color: red; height: 100vh;"></body></html>'
+    return '<html><body style="background-color: cyan; height: 100vh;"></body></html>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
